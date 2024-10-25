1e8e114b49e82efbd01328aed285b351bcbb373e
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2024-10-24 22:30:53 -0300

    Adjusted CORS and error message in register

--- bugs/Dima.Web/Configuration.cs
+++ bugs/Dima.Web/Configuration.cs
@@ -7,8 +7,7 @@ public static class Configuration
 {
     public const string HttpClientName = "dima";
     public static string StripePublicKey { get; set; } = "";
-    public static string BackendUrl { get; set; } = "http://localhost:5164";
-    
+    public static string BackendUrl { get; set; } = "https://localhost:7298";
     public static MudTheme Theme = new()
     {
         Typography = new Typography
