d67fd1909cdebed67077cf126b36288f46361dc8
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2024-10-29 07:26:04 -0300

    Implemented ShouldGenerateBase64FromString

--- unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
+++ unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
@@ -4,6 +4,14 @@ namespace Balta.Domain.Test.SharedContext.Extensions;
 
 public class StringExtensionsTests
 {
+    private const string VALUE_TO_CONVERT = "Value To Convert";
+
     [Fact]
-    public void ShouldGenerateBase64FromString() => Assert.Fail();
+    public void ShouldGenerateBase64FromString()
+    {
+        var result = StringExtensions.ToBase64(VALUE_TO_CONVERT);
+        string expectedBase64 = VALUE_TO_CONVERT.ToBase64();
+        Assert.Equal(expectedBase64, result);
+
+    }
 }
